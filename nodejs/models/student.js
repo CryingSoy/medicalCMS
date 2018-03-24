b7abc219f22ec5a4eb6cc339b2f720f49813fb68
rc <466403866@qq.com>
2018-03-24 20:47:53 +0800

    增加学生电话号码

--- nodejs/models/student.js
+++ nodejs/models/student.js
@@ -28,7 +28,7 @@ exports.querycardId = cardId => {
 exports.saveStudentInfo = data => {
   return new Promise((resolve, reject) => {
     try {
-      const sqlcommand = `insert into studentInfo(name, age, sex, height, weight, blood, room, allergy, cardId, stuId, depart) value('${data.name}', '${data.age}', '${data.sex}', '${data.height || ''}', '${data.weight || ''}', '${data.blood || ''}', '${data.room || ''}', '${data.allergy || ''}', '${data.cardId}', '${data.stuId}', '${data.depart || ''}')`
+      const sqlcommand = `insert into studentInfo(name, age, sex, height, weight, blood, room, allergy, cardId, stuId, depart, phone) value('${data.name}', '${data.age}', '${data.sex}', '${data.height || ''}', '${data.weight || ''}', '${data.blood || ''}', '${data.room || ''}', '${data.allergy || ''}', '${data.cardId}', '${data.stuId}', '${data.depart || ''}', '${data.phone || ''}')`
       mysql.mysqlConnection.query(sqlcommand, (error, rows, fields) => {
         if (error) {
           reject(error)
@@ -60,7 +60,7 @@ exports.removeStudentInfo = cardId => {
 exports.changeStudentInfo = data => {
   return new Promise((resolve, reject) => {
     try {
-      const sqlcommand = `update studentInfo set name = '${data.name}', age = '${data.age}', sex = '${data.sex}', height = '${data.height || ''}', weight = '${data.weight || ''}', blood = '${data.blood || ''}', room = '${data.room || ''}', allergy = '${data.allergy || ''}', stuId = '${data.stuId}', depart = '${data.depart || ''}' where cardId = ${data.cardId}`
+      const sqlcommand = `update studentInfo set name = '${data.name}', age = '${data.age}', sex = '${data.sex}', height = '${data.height || ''}', weight = '${data.weight || ''}', blood = '${data.blood || ''}', room = '${data.room || ''}', allergy = '${data.allergy || ''}', stuId = '${data.stuId}', depart = '${data.depart || ''}', phone = '${data.phone || ''}' where cardId = ${data.cardId}`
       mysql.mysqlConnection.query(sqlcommand, (error, rows, fields) => {
         if (error) {
           reject(error)
