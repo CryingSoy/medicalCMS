59f3869be9c5f3a71f2bca4fc9b738068042672e
CryingSoy <wgndgs@yahoo.com>
2018-04-16 23:56:08 +0800

    增加首页图表

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,9 +2,9 @@
   <div class=".dashboard-editor-container">
     <!-- <div class="dashboard-text">name:{{name}}</div>
     <div class="dashboard-text">roles:<span v-for='role in roles' :key='role'>{{role}}</span></div> -->
-    <!-- <panel-group style="padding:16px 16px 0;" @handleSetLineChartData="handleSetLineChartData"></panel-group> -->
+    <panel-group :pN="pN" :mN="mN" style="padding:16px 16px 0;" @handleSetLineChartData="handleSetLineChartData"></panel-group>
 
-    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
+    <el-row v-loading="loading" style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
       <line-chart :chart-data="lineChartData"></line-chart>
     </el-row>
   </div>
@@ -13,59 +13,9 @@
 <script>
 import { mapGetters } from 'vuex'
 import { getTreatInfoByTime } from '@/api/treat'
+import { getDrugsFlowByTime } from '@/api/drugs'
 import PanelGroup from './components/PanelGroup'
 import LineChart from './components/LineChart'
-
-const a = [6, 5, 4, 3, 2, 1, 0]
-const b = a.map(z => {
-  const c = new Date(+new Date() - z * 86400000)
-  const d = c.getHours() * 60 * 60 * 1000
-  const e = c.getMinutes() * 60 * 1000
-  const f = c.getSeconds() * 1000
-  const g = c.getMilliseconds()
-  const h = d + e + f + g
-  const i = (23 - c.getHours()) * 60 * 60 * 1000
-  const j = (59 - c.getMinutes()) * 60 * 1000
-  const k = (59 - c.getSeconds()) * 1000
-  const l = 1000 - c.getMilliseconds()
-  const m = i + j + k + l
-  return {
-    sT: +c - h,
-    eT: +c + m
-  }
-})
-
-function v(i) {
-  return new Promise((resolve, reject) => {
-    getTreatInfoByTime(i.sT, i.eT)
-      .then(res => {
-        if (res.data.code === 1) {
-          resolve(res.data.data)
-        }
-      })
-  })
-}
-
-(async function() {
-  for (const i of b) {
-    const c = await v(i)
-    if (c.length) {
-      let t = 0
-      const mT = []
-      for (const d of c) {
-        t += parseInt(d.totalPrice)
-        mT.push(d.totalPrice)
-      }
-      lineChartData.newVisitis.expectedData.push(t)
-      lineChartData.newVisitis.actualData.push(Math.max.apply(null, mT))
-      lineChartData.newVisitis.date = new Date()
-    } else {
-      lineChartData.newVisitis.expectedData.push(0)
-      lineChartData.newVisitis.actualData.push(0)
-      lineChartData.newVisitis.date = new Date()
-    }
-  }
-})()
 
 const lineChartData = {
   newVisitis: {
@@ -88,10 +38,89 @@ export default {
   },
   data() {
     return {
-      lineChartData: lineChartData.newVisitis
+      lineChartData: lineChartData.newVisitis,
+      loading: false,
+      pN: 0,
+      mN: 0,
+      xArray: []
     }
   },
+  mounted() {
+    this.loading = true
+    const a = [6, 5, 4, 3, 2, 1, 0]
+    const b = a.map(z => {
+      const c = new Date(+new Date() - z * 86400000)
+      const d = c.getHours() * 60 * 60 * 1000
+      const e = c.getMinutes() * 60 * 1000
+      const f = c.getSeconds() * 1000
+      const g = c.getMilliseconds()
+      const h = d + e + f + g
+      const i = (23 - c.getHours()) * 60 * 60 * 1000
+      const j = (59 - c.getMinutes()) * 60 * 1000
+      const k = (59 - c.getSeconds()) * 1000
+      const l = 1000 - c.getMilliseconds()
+      const m = i + j + k + l
+      return {
+        sT: +c - h,
+        eT: +c + m
+      }
+    })
+    
+    if (1) {
+      (async() => {
+        for (const i of b) {
+          const c = await this.refresh(i)
+          if (c.length) {
+            let t = 0
+            const mT = []
+            for (const d of c) {
+              t += parseInt(d.totalPrice)
+              mT.push(d.totalPrice)
+            }
+            lineChartData.newVisitis.expectedData.push(t)
+            lineChartData.newVisitis.actualData.push(Math.max.apply(null, mT))
+            lineChartData.newVisitis.date = new Date()
+          } else {
+            lineChartData.newVisitis.expectedData.push(0)
+            lineChartData.newVisitis.actualData.push(0)
+            lineChartData.newVisitis.date = new Date()
+          }
+
+          const d = await this.pNum(i)
+          if (d.length) {
+            this.xArray.push(d.length)
+          } else {
+            this.xArray.push(0)
+          }
+          this.pN = this.xArray[6]
+        }
+        this.loading = false
+        this.mN = lineChartData.newVisitis.expectedData[6]
+      })()
+    }
+
+  },
   methods: {
+    refresh(i) {
+      return new Promise((resolve, reject) => {
+        getTreatInfoByTime(i.sT, i.eT)
+          .then(res => {
+            if (res.data.code === 1) {
+              resolve(res.data.data)
+            }
+          })
+      })
+    },
+    pNum(i) {
+      return new Promise((resolve, reject) => {
+        getTreatInfoByTime(i.sT, i.eT)
+          .then(res => {
+            if (res.data.code === 1) {
+              resolve(res.data.data)
+            }
+          })
+      })
+    },
     handleSetLineChartData(type) {
       this.lineChartData = lineChartData[type]
     }
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,7 +2,7 @@
   <div class="login-container">
     <el-form autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left" label-width="0px"
       class="card-box login-form">
-      <h3 class="title">高校医疗信息管理系统后台</h3>
+      <h3 class="title">高校医疗信息管理系统</h3>
       <el-form-item prop="username">
         <span class="svg-container svg-container_login">
           <svg-icon icon-class="user" />
@@ -52,8 +52,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin',
-        password: 'qweasd'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
